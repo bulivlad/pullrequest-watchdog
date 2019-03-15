67c211e4a16fbafc2f5c969cf75c222fef02bd8a
Vlad Bulimac <vbulimac@deloittece.com>
2019-03-15 16:37:41 +0200

    improve the healthcheck slack controller mapping

--- src/main/java/io/watchdog/pullrequest/model/slack/SlackEventMapping.java
+++ src/main/java/io/watchdog/pullrequest/model/slack/SlackEventMapping.java
@@ -17,7 +17,7 @@ public enum SlackEventMapping {
     ADD_TEAM_EVENT_REGEX ("(?i)(add team\\s)(?<teamName>[A-Za-z0-9\\-\\_\\.]+).*(members\\s\\[)(?<members>.*)(\\]).*(and\\sscheduler\\s)(?<schedulerExpression>.*)"),
     REMOVE_TEAM_EVENT_REGEX ("(?i)(remove team\\s)(?<teamName>.*)"),
     UNSCHEDULE_TEAM_EVENT_REGEX ("(?i)(unschedule team\\s)(?<teamName>.*)"),
-    HEALTH_CHECK_MESSAGE_REGEX ("([A-Za-z\\s].*)(?i)(healthcheck)([A-Za-z\\s].*)");
+    HEALTH_CHECK_MESSAGE_REGEX ("([A-Za-z\\s]*)(?i)(healthcheck)([A-Za-z\\s]*)");
 
     @Getter
     String value;
